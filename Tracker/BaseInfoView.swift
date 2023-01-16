ad23591a4a11c024ce273b8e54728585e467397f
KodAndN <andrkokaz@gmail.com>
2023-01-16 18:36:28 +0300

    Дополнение конфигуратора View

--- Tracker/BaseInfoView.swift
+++ Tracker/BaseInfoView.swift
@@ -16,6 +16,12 @@ class BaseInfoView: BaseView {
         return lable
     }()
     
+    private let button: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .red
+        return button
+    }()
+    
     private let contentView: UIView = {
         let view = UIView()
         view.backgroundColor = .white
@@ -25,15 +31,21 @@ class BaseInfoView: BaseView {
         return view
     }()
         
-    init(with title: String? = nil, alignment: NSTextAlignment = .left) {
+    init(with title: String? = nil, buttonTitle: String? = nil) {
         titleLabel.text = title?.uppercased()
-        titleLabel.textAlignment = alignment
+        titleLabel.textAlignment = buttonTitle == nil ? .center : .left
         
+        button.setTitle(buttonTitle, for: .normal)
+        button.isHidden = buttonTitle == nil ? true : false
         super.init(frame: .zero)
     }
     
     required init?(coder: NSCoder) {
         super.init(frame: .zero)
+    }
+    
+    func addButtonTarget(target: Any?, action: Selector) {
+        button.addTarget(action, action: action, for: .touchUpInside)
     }
 }
 
@@ -42,6 +54,7 @@ extension BaseInfoView {
         super.setupViews()
         
         setupView(titleLabel)
+        setupView(button)
         setupView(contentView)
         
     }
@@ -56,6 +69,11 @@ extension BaseInfoView {
             titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
             titleLabel.topAnchor.constraint(equalTo: topAnchor),
             titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+            
+            button.trailingAnchor.constraint(equalTo: trailingAnchor),
+            button.centerYAnchor.constraint(equalTo: titleLabel.centerYAnchor),
+            button.widthAnchor.constraint(equalToConstant: 130),
+            button.heightAnchor.constraint(equalToConstant: 30),
             
             contentView.topAnchor.constraint(equalTo: contentTopAnchor, constant: contentTopOffset),
             contentView.leadingAnchor.constraint(equalTo: leadingAnchor),
